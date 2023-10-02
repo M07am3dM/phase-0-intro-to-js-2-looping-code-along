53b4be789aed2821e5f93755cca9f439a9b06613
mohamed <matassi.mohamed89@gmail.com>
2023-10-03 00:10:49 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const writeCards = (people, event) => {
+    let cards = []; 
+
+    people.forEach(peopleName => {
+        cards.push(`Thank you, ${peopleName}, for the wonderful ${event} gift!`);
+    });
+
+    return cards;
+};
+
+const countDown = (number) => {
+    while (number >=0) {
+        console.log(number);
+        --number;
+        
+    }
+}
+
+countDown(10);
